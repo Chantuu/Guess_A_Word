c88ef8fbd0b9adb637254585e908ac93bb6fe251
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-07-05 21:59:01 +0400

    Define necessary variables for game logic

---
+++ javascripts/app.js
@@ -0,0 +1,7 @@
+// Definition of necessary elements to interact with
+const checkButton = document.querySelector('.btn-custom.check');
+const restartButton = document.querySelector('.btn-custom.restart');
+const gameInputContainer = document.querySelector('.gameInputContainer');
+
+// Definition of Word Array
+const words = ['გემი', 'მანქანა', 'დედა', 'ტელეფონი', 'ზარი', 'ქალაქი', 'სოფელი'];
