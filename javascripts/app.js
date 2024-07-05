1dd1c5692b11c1c2242d945690c6f3616543abbd
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-07-06 00:07:38 +0400

    Add random word picker function
     Added function, which picks random word from the words array.

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,3 +5,11 @@ const gameInputContainer = document.querySelector('.gameInputContainer');
 
 // Definition of Word Array
 const words = ['გემი', 'მანქანა', 'დედა', 'ტელეფონი', 'ზარი', 'ქალაქი', 'სოფელი'];
+
+
+// This function return random word from the 'words' array
+function selectRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+
+    return words[randomIndex];
+}
